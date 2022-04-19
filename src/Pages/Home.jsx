6b67ad8ddf1c8278083b9beacbc4602401289383
Jose Ugarte <josedub93@gmail.com>
2022-04-18 20:11:33 -0400

    avance

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function Home (){
-return <h2>hola</h2>
+
+
+
+
+return (
+<div className="App">
+<h2>hola</h2>
+                           
+            
+          
+             <form className='formulario'>
+                <textarea
+                     name="tweet"
+                     
+                     cols="30"
+                     rows="5"
+                     placeholder="escribe un tweet..."
+                 />
+                 <div className='input-group'>
+
+                     <button>Enviar tweet</button>
+                 </div>
+             </form>
+            <h1>Tweets:</h1>
+         
+         </div>
+)
 };
 
 export default Home;
--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,11 +1,35 @@
-import {loginWithGoogle, auth, logout} from "../firebase";
+import React, {useContext, useState} from 'react';
 import { AppContext } from "../AppContext/AppContext";
+import { firestore, loginWithGoogle } from "../firebase";
+import bigLogo from "../Icons/logo big.svg";
+import googleSignIn from "../Icons/google sign in.svg";
 
-function SignIn(){
-return(
-    <div>
-   <h2>Sign in</h2>
-    </div>
-)
-}
-export default SignIn;
+function SignIn() {
+
+  
+       return (
+         <div>
+           <div>
+             <img src={bigLogo} alt="devs_united logo"/>
+           </div>
+           <div>
+             <h1>Welcome!</h1>
+             <p>
+               The place where developeres from all around the world share thoughts,
+               ideas, get inspired and so much more.
+             </p>
+             <img
+               src={googleSignIn}
+               alt="google sign in"
+               
+               onClick={loginWithGoogle}
+             />
+             <p>
+               © 2020 Devs_United - <span>BETA</span>
+             </p>
+           </div>
+         </div>
+       );
+     };
+     
+     export default SignIn;
